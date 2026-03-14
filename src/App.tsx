9691b776d543e503e3371c1bc63eee605b89638b
Vinicius Milani <vmrfreitas@inf.ufrgs.br>
2026-03-14 00:07:53 -0300

    adding my name just because

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Vinicius Milani</h1>
           <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+            Welcome to my <code>website</code> 😌
           </p>
         </div>
         <button
